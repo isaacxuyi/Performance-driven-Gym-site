770eff552a825aa42afb1b2638fd5937aceeb078
Isaac Uhunmwangho <isaacco911@gmail.com>
2025-09-24 17:07:42 +0100

    header update 'union icon for mobile'

--- driven/src/app/components/Header.js
+++ driven/src/app/components/Header.js
@@ -1,5 +1,6 @@
 "use client";
 import logo from '../Images/Vector.svg'
+import union from '../Images/Union.svg'
 import Image from 'next/image';
 import Link from 'next/link';
 import { useState } from 'react';
@@ -8,13 +9,16 @@ export default function Header() {
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <div className="w-full px-4 py-3 flex flex-wrap md:flex-nowrap justify-between items-center bg-neutral-100 gap-y-3">
-      <div className="flex items-center mb-2 md:mb-0">
+    <div className="w-full px-4 py-3 flex justify-between items-center bg-neutral-100">
+      {/* Logo */}
+      <div className="flex items-center">
         <Image height={40} width={40} src={logo} alt='logo' />
         <p className='ml-1 font-bold'>PrimalTraining</p>
       </div>
-      <nav className="w-full md:w-auto">
-        <ol className='hidden md:flex flex-wrap justify-center md:justify-end gap-3 md:gap-5 items-center'>
+      {/* Navigation */}
+      <nav className="flex items-center">
+        {/* Desktop nav */}
+        <ol className='hidden md:flex flex-wrap justify-end gap-3 md:gap-5 items-center'>
           <li><Link href="/">HOME</Link></li>
           <li><Link href="/about">ABOUT</Link></li>
           <li>
@@ -26,6 +30,10 @@ export default function Header() {
             </button>
           </li>
         </ol>
+        {/* Mobile union icon */}
+        <button className="block md:hidden ml-4" aria-label="Open menu">
+          <Image height={40} width={40} src={union} alt='unionicon'/>
+        </button>
       </nav>
     </div>
   );
--- driven/src/app/layout.js
+++ driven/src/app/layout.js
@@ -9,7 +9,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className="min-w-[320px]">
         <Header />
         {children}
       </body>
